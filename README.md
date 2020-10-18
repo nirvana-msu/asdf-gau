52574f9fc56f6ab224fc4238eb3de3f82033cbdc
nirvana-msu <student_vmk@mail.ru>
2020-10-18 13:50:57 +0100

    First cut of gau asdf-vm plugin.

---
+++ README.md
@@ -0,0 +1,14 @@
+# asdf-gau
+
+[gau](https://github.com/lc/gau) plugin for [asdf](https://github.com/asdf-vm/asdf) version manager.
+
+## Install
+
+```
+asdf plugin-add gau git@github.com:nirvana-msu/asdf-gau.git
+```
+Windows installation requires `unzip`, all other OS require `tar` to be present.
+
+## Use
+
+Check [asdf](https://github.com/asdf-vm/asdf) documentation for instructions on how to install & manage tool versions.
---
+++ bin/util.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+getOs() {
+    WIN=( "msys" "cygwin" "win32" )
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        echo "linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "macOS"
+    elif [[ " ${WIN[@]} " =~ " $OSTYPE " ]]; then
+        echo "windows"
+    elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        echo "freebsd"
+    elif [[ "$OSTYPE" == "openbsd"* ]]; then
+        echo "openbsd"
+    else
+        echo "Unsupported platform!"
+        exit 1
+    fi
+}
+
+getPlatform() {
+    # This assumes amd64 or 386 only. It needs to be updated to support ARM as well!
+    if [[ `getconf LONG_BIT` = "64" ]]; then
+        arch="amd64"
+    else
+        arch="386"
+    fi
+    echo "$(getOs)_${arch}"
+}
+
+queryReleases() {
+    releases_path=https://api.github.com/repos/lc/gau/releases
+    cmd="curl -sS"
+    if [[ -n "${GITHUB_API_TOKEN}" ]]; then
+        cmd="${cmd} -H 'Authorization: token ${GITHUB_API_TOKEN}'"
+    fi
+    eval "${cmd} ${releases_path}"
+}
